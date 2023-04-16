7a9558fa9820f31483849d0b05d76f42e35db75c
Iaguin11 <110638131+Iaguin11@users.noreply.github.com>
2023-04-16 19:38:48 -0300

    second

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ function birthaday(date){
     const ageInYears = today.diff(birthaday, 'year')
     const nextBirthaday = birthaday.add(ageInYears + 1 , 'year')
     const daysToNextBirthday = nextBirthaday.diff(today, 'day') + 1
+    const hour = dayjs().hour()
+    const minutes = dayjs().minute()
+    const seconds = dayjs().second()
 
     console.log(`Idade : ${ageInYears}`)
     console.log(`Próximo aniversário : ${nextBirthaday.format('DD/MM/YYYY')}`)
     console.log(`Dias até completar ${ageInYears + 1} anos : ${daysToNextBirthday}`)
+    console.log(`Hora atual : ${hour}:${minutes}:${seconds}`)
+
 }
 
 birthaday('2001-11-07')
